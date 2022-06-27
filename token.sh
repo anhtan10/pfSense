86ae510b10a64e2426bdce4e66871aad72436363
Anh Tan Le <anh.le@stud.tfbern.ch>
2022-06-27 09:26:03 +0200

    no message

--- token.sh
+++
@@ -1,2 +0,0 @@
-curl -H 'Authorization: token <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>' <https://raw.githubusercontent.com/anhtan10/pfSense/master/test.txt>
-curl -v -o mysh.sh -H 'Authorization: token <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>' https://raw.githubusercontent.com/anhtan10/pfSense/master/test.txt?token=GHSAT0AAAAAABV2MTAEO5VTQV7PMDYDPAW2YVS3W2A
